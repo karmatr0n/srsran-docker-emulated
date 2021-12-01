0e56ee6e298e0261fe46d952f5634802d57b1dec
jwijenbergh <jeroenwijenbergh@protonmail.com>
2021-12-01 15:00:56 +0100

    Script: early exit if fail and add log info

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose kill
-docker-compose up -d
+docker-compose up -d || exit
 
 printf "%s" "Waiting for UE to attach"
 while ! docker exec -t -t virtual-srsepc timeout 0.2 ping -c 1 -n 172.16.0.2 &> /dev/null
@@ -15,3 +15,4 @@ docker exec virtual-srsepc iptables -t nat -A POSTROUTING -s 172.16.0.0/24 -o et
 docker exec virtual-srsue ip route replace default via 172.16.0.1
 
 printf "%s\n" "Configured, test with: docker exec virtual-srsue ping example.com"
+printf "%s\n" "The logs can be accessed and followed with: docker-compose logs -f"
